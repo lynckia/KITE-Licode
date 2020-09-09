e2d276a670cdd7bde1f039c3da61ba9f2f0e6631
Pedro Rodriguez <lodoyun@gmail.com>
2020-09-09 16:34:47 +0200

    Update KITE-Licode-Test/js/pages/MainPage.js
    Co-authored-by: Javier Cerviño <jcague@gmail.com>

--- KITE-Licode-Test/js/pages/MainPage.js
+++ KITE-Licode-Test/js/pages/MainPage.js
@@ -84,7 +84,8 @@ class MainPage {
     let timeout = stepInfo.timeout;
     stepInfo.numberOfParticipant = parseInt(stepInfo.numberOfParticipant); // To add the first video
     console.warn(`Testing Audio with ${stepInfo.numberOfParticipant} participants, index`, index);
-    await TestUtils.waitForVideos(stepInfo, audios);
+    await TestUtils.waitForAudios(stepInfo, audios);
+
     // Waiting for all the videos
 
     return true;
